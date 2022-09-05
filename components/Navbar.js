9f7029f6454157286fb741d50f7af4d87c16d97e
anirudh gautam <anirudhgautam987@gmail.com>
2022-09-06 00:41:21 +0530

    sidebar added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
-
 import { BsCart2 } from "react-icons/bs";
+import {
+  AiOutlineCloseCircle,
+  AiFillPlusCircle,
+  AiFillMinusCircle,
+} from "react-icons/ai";
 
 const Navbar = () => {
+  const toggleCart = () => {
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else {
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
+    }
+  };
+  const ref = useRef();
   return (
     <div className="flex flex-col md:flex-row md:justify-start justify-between items-center shadow-xl">
       <div className="logo m-2">
@@ -36,8 +50,45 @@ const Navbar = () => {
           </Link>
         </ul>
       </div>
-      <div className="cart absolute right-5 top-4">
+      <div
+        onClick={toggleCart}
+        className="cart absolute right-5 top-4 cursor-pointer"
+      >
         <BsCart2 className="text-2xl md:text-4xl" />
+      </div>
+      <div
+        ref={ref}
+        className="w-60 h-full z-10 sidebar absolute top-0 right-0 bg-blue-200 py-10 px-6 transform transition-transform translate-x-full"
+      >
+        <h2 className="font-bold text-xl">Your cart</h2>
+        <span
+          onClick={toggleCart}
+          className="absolute top-2 right-2 cursor-pointer text-2xl"
+        >
+          <AiOutlineCloseCircle />
+        </span>
+        <ol className="list-decimal font-semibold">
+          <li>
+            <div className="item flex my-3">
+              <div className="w-2/3 font-semibold">The Catalyzer</div>
+              <div className="w-1/3 flex items-center justify-center text-lg">
+                <AiFillMinusCircle className="cursor-pointer" /> <span className="mx-2">1</span>
+                <AiFillPlusCircle className="cursor-pointer" />
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="item flex my-3">
+              <div className="w-2/3 font-semibold">Shooting Stars</div>
+              <div className="w-1/3 flex items-center justify-center text-lg">
+                <AiFillMinusCircle className="cursor-pointer" /> <span className="mx-2">1</span>
+                <AiFillPlusCircle className="cursor-pointer" />
+              </div>
+            </div>
+          </li>
+        </ol>
+        <button className="flex mx-auto text-white bg-black/80 border-0 py-2 px-8 focus:outline-none hover:bg-black rounded text-lg">Proceed To Buy</button>
+      
       </div>
     </div>
   );
--- pages/Tshirts.js
+++ pages/Tshirts.js
@@ -1,96 +1,177 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Tshirts = () => {
   return (
     <div>
       <section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-wrap -m-4">
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"/>
-        </a>
-        <div className="mt-4">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-          <p className="mt-1">$16.00</p>
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <Link href={"/Product/wear-the-code"}>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg ">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/Product/wear-the-code"}>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg ">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Shooting Stars
+                  </h2>
+                  <p className="mt-1">$21.15</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/Product/wear-the-code"}>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg ">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Neptune
+                  </h2>
+                  <p className="mt-1">$12.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/Product/wear-the-code"}>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg ">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The 400 Blows
+                  </h2>
+                  <p className="mt-1">$18.40</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/Product/wear-the-code"}>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg ">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/Product/wear-the-code"}>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg ">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Shooting Stars
+                  </h2>
+                  <p className="mt-1">$21.15</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/Product/wear-the-code"}>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg ">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Neptune
+                  </h2>
+                  <p className="mt-1">$12.00</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/Product/wear-the-code"}>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg ">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The 400 Blows
+                  </h2>
+                  <p className="mt-1">$18.40</p>
+                </div>
+              </div>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"/>
-        </a>
-        <div className="mt-4">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p className="mt-1">$21.15</p>
-        </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"/>
-        </a>
-        <div className="mt-4">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p className="mt-1">$12.00</p>
-        </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"/>
-        </a>
-        <div className="mt-4">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p className="mt-1">$18.40</p>
-        </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"/>
-        </a>
-        <div className="mt-4">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-          <p className="mt-1">$16.00</p>
-        </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"/>
-        </a>
-        <div className="mt-4">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p className="mt-1">$21.15</p>
-        </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"/>
-        </a>
-        <div className="mt-4">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p className="mt-1">$12.00</p>
-        </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571786_6023057.jpg?format=webp&w=360&dpr=1.5"/>
-        </a>
-        <div className="mt-4">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p className="mt-1">$18.40</p>
-        </div>
-      </div>
+      </section>
     </div>
-  </div>
-</section>
-    </div>
-  )
-}
+  );
+};
 
-export default Tshirts
+export default Tshirts;
