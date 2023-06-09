97d087a0d9acd85f043975524f60b9266fad7c95
anirudh gautam <anirudhgautam987@gmail.com>
2023-06-09 14:45:07 +0530

    env changed

--- pages/login.js
+++ pages/login.js
@@ -129,7 +129,7 @@ const Login = () => {
                 </button>
                 <p className="text-center text-black my-4">
                   Don&apos;t have an account?{" "}
-                  <Link href={"/SignUp"}>
+                  <Link href={"/signup"}>
                     <a className="border-b border-grey-dark text-blue-600">
                       Sign Up
                     </a>
