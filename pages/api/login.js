95e5eaa649f0fbc5d22404a0b30094f7fae28be7
anirudh gautam <anirudhgautam987@gmail.com>
2023-06-09 13:27:19 +0530

    cors error on prod

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,9 +4,19 @@ var CryptoJS = require("crypto-js");
 var jwt = require("jsonwebtoken");
 
 const handler = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://codeswear-gamma.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://codeswear-gamma.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    // Handle preflight request
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === "POST") {
     console.log(req.body);
     let user = await User.findOne({ email: req.body.email });
