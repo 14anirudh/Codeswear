9985346e4b55c86d19c6db4fd284ab5eb99181f8
anirudh gautam <anirudhgautam987@gmail.com>
2023-07-22 01:52:17 +0530

    debug prod

--- pages/api/login.js
+++ pages/api/login.js
@@ -6,7 +6,9 @@ var jwt = require("jsonwebtoken");
 const handler = async (req, res) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://codeswear-gamma.vercel.app"
+    // "https://codeswear-gamma.vercel.app"
+    "https://radagast.vercel.app.app"
+
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
