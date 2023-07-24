c2636a74d1ba884f58a204c4c8d60a1a40ce48de
anirudh gautam <anirudhgautam987@gmail.com>
2023-07-25 01:57:14 +0530

    debug prod apis

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,32 +4,17 @@ var CryptoJS = require("crypto-js");
 var jwt = require("jsonwebtoken");
 
 const handler = async (req, res) => {
-  // res.setHeader(
-  //   "Access-Control-Allow-Origin",
-  //   // "https://codeswear-gamma.vercel.app"
-  //   "https://radagast.vercel.app.app"
-
-  // );
-  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  // if (req.method === "OPTIONS") {
-  //   // Handle preflight request
-  //   res.status(200).end();
-  //   return;
-  // }
-
   if (req.method === "POST") {
-
+    console.log(req.body);
     let user = await User.findOne({ email: req.body.email });
-    const bytes = CryptoJS.AES.decrypt(user.password,process.env.AES_SECRET);
-  
+    const bytes = CryptoJS.AES.decrypt(user.password, "secret");
+    console.log(bytes.toString(CryptoJS.enc.Utf8));
     let originalText = bytes.toString(CryptoJS.enc.Utf8);
     if (user) {
       if (req.body.email == user.email && req.body.password == originalText) {
         var token = jwt.sign(
           { email: user.email, name: user.name },
-          process.env.JWT_SECRET,
+          "jwtsecret",
           { expiresIn: "24h" }
         );
         res.status(200).json({ Success: "success", token });
